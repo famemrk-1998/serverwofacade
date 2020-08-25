a519d5983d410a6ed2e812f167e19c0b5e3c51e9
famemrk-1998 <Jetnipit Morakot@LAPTOP-I8E0LR7T>
2020-08-25 15:08:08 +0700

    Change by Jetnipit Morakot

---
+++ src/serverwofacade/MyFacade.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverwofacade;
+
+/**
+ *
+ * @author Jetnipit Morakot
+ */
+public class MyFacade {
+    
+    private static MyFacade MyFacadeObject = null;
+    private MyFacade(){}
+    private ScheduleServer scheduleServer = new ScheduleServer();
+    public static MyFacade getMyFacadeObject()
+    {
+        if(MyFacadeObject == null)
+        {MyFacadeObject = new MyFacade();}
+        
+        return MyFacadeObject;
+    }
+    
+    public void StartServer()
+    {
+	scheduleServer.startBooting();
+	scheduleServer.readSystemConfigFile();
+	scheduleServer.init();
+	scheduleServer.initializeContext();
+	scheduleServer.initializeListeners();
+	scheduleServer.createSystemObjects();
+	System.out.println("Start working......");
+    }
+    
+    public  void StopServer()
+    {
+        System.out.println("After work done.........");
+	scheduleServer.releaseProcesses();
+	scheduleServer.destory();
+	scheduleServer.destroySystemObjects();
+	scheduleServer.destoryListeners();
+	scheduleServer.destoryContext();
+	scheduleServer.shutdown();
+    }
+    
+}
